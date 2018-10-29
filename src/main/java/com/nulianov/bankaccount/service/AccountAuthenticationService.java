d886b4267d7d70cc31a61d87377cb2c29b6052ed
Nikita Ulyanov <nikita.ulyanov@netcracker.com>
2018-10-29 17:32:36 +0300

    [add] javadocs

--- src/main/java/com/nulianov/bankaccount/service/AccountAuthenticationService.java
+++ src/main/java/com/nulianov/bankaccount/service/AccountAuthenticationService.java
@@ -7,7 +7,20 @@ import java.util.Optional;
 
 @Service
 public interface AccountAuthenticationService {
+    /**
+     * If received credentials fit to data in storage, generates and returns auth token,
+     * returns empty token otherwise
+     * @param username user name from request parameters
+     * @param password password from request parameters
+     * @return auth token
+     */
     Optional<String> login(String username, String password);
 
+    /**
+     * If received token is present in storage, returns correlated account,
+     * returns empty optional otherwise
+     * @param token token from request header
+     * @return account linked with specified token
+     */
     Optional<Account> findByToken(String token);
 }
--- src/main/java/com/nulianov/bankaccount/service/AccountService.java
+++ src/main/java/com/nulianov/bankaccount/service/AccountService.java
@@ -11,11 +11,42 @@ import java.util.List;
 
 @Service
 public interface AccountService {
+    /**
+     * If account exists in storage, returns actual balance,
+     * throws exception otherwise
+     * @param username username of current logged user
+     * @return user account balance
+     * @throws EntityNotFoundException account was not found in storage
+     */
     BigDecimal getBalance(String username) throws EntityNotFoundException;
 
+    /**
+     * If account exists in storage, returns statement as list of transactions,
+     * throws exception otherwise
+     * @param username username of current logged user
+     * @return list of transactions
+     * @throws EntityNotFoundException account was not found in storage
+     */
     List<TransactionDetails> getStatement(String username) throws EntityNotFoundException;
 
+    /**
+     * If account exists in storage and amount of money to deposit is correct, processes deposit and returns actual balance,
+     * throws exception otherwise
+     * @param transactionDetails details about transaction
+     * @return user account balance
+     * @throws EntityNotFoundException account was not found in storage
+     * @throws IllegalAmountOfMoneyForTransaction amount of money to deposit is incorrect
+     */
     BigDecimal deposit(TransactionDetails transactionDetails) throws EntityNotFoundException, IllegalAmountOfMoneyForTransaction;
 
+    /**
+     * If account exists in storage, has sufficient funds and amount of money to withdraw is correct,
+     * processes deposit and returns actual balance, throws exception otherwise
+     * @param transactionDetails details about transaction
+     * @return user account balance
+     * @throws EntityNotFoundException account was not found in storage
+     * @throws IllegalAmountOfMoneyForTransaction amount of money to deposit is incorrect
+     * @throws InsufficientFundsException account doesn't have sufficient funds to withdraw
+     */
     BigDecimal withdraw(TransactionDetails transactionDetails) throws EntityNotFoundException, IllegalAmountOfMoneyForTransaction, InsufficientFundsException;
 }
